e15a8cc0369f14bd77941118b980a3c805e065a1
Wasiu-hub <contactbow@yahoo.com>
2023-12-22 11:32:51 +0000

    web page loaded and cookies accepted

--- src/test/java/com/next/Search.java
+++ src/test/java/com/next/Search.java
@@ -3,11 +3,26 @@ package com.next;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class Search {
 
     @Given("I am on the next home page")
     public void i_am_on_the_next_home_page() {
+
+        WebDriverManager.chromedriver().setup(); // set up the driver
+        WebDriver driver = new ChromeDriver(); // creates browser instance
+//      driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        driver.get("https://www.next.co.uk/");
+
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+
 
     }
     @When("I enter {string} as a product")
